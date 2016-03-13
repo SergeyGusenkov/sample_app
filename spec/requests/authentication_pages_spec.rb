d8dab8ccef0c937707c93bcf82e261845170ad6e
Sergey Gusenkov <gusenkov.sergei@gmail.com>
2016-03-13 13:23:15 +0300

    Finish sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "AuthenticationPages", type: :request do
+  
+   subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+  
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Быстрее зайти и наслаждаться" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+    end
+  end
+end
